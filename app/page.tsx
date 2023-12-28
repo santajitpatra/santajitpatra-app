8e7e3809f1eaa618a8c2a37760affa01b3f89b2c
Santajit Patra <business.santajitpatra@gmail.com>
2023-12-28 23:57:45 +0530

    feat: added all image and Hero section
    feat: added all image and Hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
+import Hero from "@/components/Hero";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import React from "react";
 
 const Home = () => {
   return (
     <>
-      <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
-        <h1 className="text-center text-5xl">HOME</h1>
+      <MaxWidthWrapper >
+        <Hero />
       </MaxWidthWrapper>
     </>
   );
