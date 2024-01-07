69ac1e774f8e7df17c1130346b1ec31b3561f7b7
Santajit Patra <business.santajitpatra@gmail.com>
2024-01-08 02:34:13 +0530

    SEO done
    SEO done

---
+++ lib/customMetadata.ts
@@ -0,0 +1,46 @@
+import { Metadata } from "next";
+
+// metadata
+export function customMetadata({
+  title = "Santajit Patra - The SaaS for Everyone",
+  description = "Expert web development and digital marketing solutions for business success.",
+  image = "/thumbnail.png",
+  icons = "/favicon.ico",
+  noIndex = false,
+}: {
+  title?: string;
+  description?: string;
+  image?: string;
+  icons?: string;
+  noIndex?: boolean;
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [
+        {
+          url: image,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [image],
+      creator: "@santajitpatra",
+    },
+    icons,
+    metadataBase: new URL("https://santajitpatra.vercel.app"),
+    themeColor: "#FFF",
+    ...(noIndex && {
+      robots: {
+        index: false,
+        follow: false,
+      },
+    }),
+  };
+}
