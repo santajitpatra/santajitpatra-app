fce92e07a47fe66cbca9b03e27bf44abfddebb9a
Santajit Patra <business.santajitpatra@gmail.com>
2024-01-07 23:55:49 +0530

    feat : added googleTagManager
    feat : added googleTagManager

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,19 +23,28 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         <Script
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
-          strategy="afterInteractive"
+          id="show-banner"
+          dangerouslySetInnerHTML={{
+            __html: `
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID}');
+`,
+          }}
         />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}');
-          `}
-        </Script>
       </head>
       <body className={`bg-[#0a162f] ${inter.className}`}>
+        <Script
+          id="show-banner"
+          dangerouslySetInnerHTML={{
+            __html: `
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID}"
+  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+`,
+          }}
+        />
         {/* <ThemeProvider
           attribute="class"
           defaultTheme="system"
