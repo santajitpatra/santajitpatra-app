cd2b498ff22ba9c8c4e9e03155e0bfacb3363aec
Santajit Patra <business.santajitpatra@gmail.com>
2024-01-29 03:40:14 +0530

    feat : mdx blog feater added
    feat : mdx blog feature added  but not working  properly

---
+++ app/(marketing)/blog/page.tsx
@@ -0,0 +1,31 @@
+import { getAllPostsMeta } from '@/lib/mdx';
+import Link from 'next/link'
+
+const Page = async () => { 
+    const posts = await getAllPostsMeta();
+
+return (
+  <section className="py-24">
+    <div className="container">
+      <h1 className="text-3x1 font-bold">All Posts</h1>
+      <div className="flex gap-6 mt-6">
+        {posts?.map((post) => (
+          <Link
+            href={`posts/${post.slug}`}
+            key={post?.title}
+            className="p-8 rounded-md shadow-md"
+          >
+            <h3 className="text-xl font-semibold">{post.title}</h3>
+            <p className="mt-4 text-sm">{post.author}</p>
+            <time className="text-[12px] text-gray-400">
+              {post.publishDate}
+            </time>
+          </Link>
+        ))}
+      </div>
+    </div>
+  </section>
+);
+    }
+    
+    export default Page
--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
+const withMDX = require("@next/mdx")();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Configure `pageExtensions` to include MDX files
+  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+  // Optionally, add any other Next.js config below
   images: {
     domains: ["images.unsplash.com", "images.pexels.com"],
   },
 };
 
-module.exports = nextConfig
+module.exports = withMDX(nextConfig);
