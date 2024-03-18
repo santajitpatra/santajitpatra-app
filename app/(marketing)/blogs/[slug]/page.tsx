61d4cb36555651e4bb897cd30ec4b623bec648d0
Santajit Patra <121734312+santajitpatra@users.noreply.github.com>
2024-03-19 02:31:44 +0530

    feat: Created blog post for users on Resendcom's authentication
    feat: Created blog post for users on Resendcom's authentication
    
    This commit introduces a new blog post titled "Unlocking the Power of Resendcom: Seamless Authentication Solutions" in the `content/blogs` directory.
    
    The post caters to users seeking information about easier and more convenient authentication methods.

--- app/(marketing)/blogs/[slug]/page.tsx
+++ app/(marketing)/blogs/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import Markdown from "markdown-to-jsx";
 import { getPostContent, getPostMetadata } from "@/lib/mdx";
 import BlogCTA from "@/components/BlogCTA";
+import Image from "next/image";
 
 export const generateStaticParams = async () => {
   const posts = getPostMetadata();
@@ -20,6 +21,13 @@ const PostPage = (props: any) => {
           <h1 className="text-2xl text-slate-200 ">{post.data.title}</h1>
           <p className="text-slate-400 mt-2">{post.data.date}</p>
         </div>
+        <Image
+          src={post.data.thumbnail || "/slide/slide1.jpg"}
+          className="rounded-xl m-auto mt-10"
+          width={1000}
+          height={1000}
+          alt="Picture of the author"
+        />
         <div className="container pt-20 prose lg:prose-xl text-slate-400 bg-gradient-to-r from-cyan-700 to-cyan-400 bg-clip-text prose-headings:text-transparent prose-a:text-cyan-500 prose-a:underline prose-strong:text-slate-400 prose-code:text-slate-400 prose-p:text-slate-400">
           <Markdown>{post.content}</Markdown>
         </div>
